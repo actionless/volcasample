64b69e05c8513c87a05ae6dc1a6f6c694c7a5bd1
Tundish <tundish@thuswise.org>
2017-01-04 19:21:26 +0000

    Added syro module.

---
+++ volcasample/syro.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+"""
+uint32_t SyroComp_GetCompSize(const uint8_t *psrc, uint32_t num_of_sample,
+	uint32_t quality, Endian sample_endian);
+
+uint32_t SyroComp_Comp(const uint8_t *psrc, uint8_t *pdest, int num_of_sample, 
+	int quality, Endian sample_endian);
+"""
+
+
