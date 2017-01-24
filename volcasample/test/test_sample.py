d8aadcb8f30cdc7df5a3ba8c3f443cc1da939b08
tundish <tundish@thuswise.org>
2017-01-24 13:41:37 +0000

    Test module for samples.

---
+++ volcasample/test/test_sample.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# encoding: UTF-8
+
+import unittest
+
+import pkg_resources
+
+class ConversionTests(unittest.TestCase):
+
+    def test_stereo_to_mono(self):
+        self.fail()
+
