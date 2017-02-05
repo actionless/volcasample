15a8b52dd163915177d2da41e6764a5a339e9e4b
Tundish <tundish@thuswise.org>
2017-02-05 20:05:54 +0000

    setup.py install works on Linux from sdist.

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
             "doc/html/_static/*.gif",
             "doc/html/_static/*.js",
             "doc/html/_static/*.png",
+            "lib/*",
             "../COPYING",
             "../syro/*.h",
             "../syro/*.c",
